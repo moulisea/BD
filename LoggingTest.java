17cd35b3083ca581b0f6fa2f2d87d89028fb34c2
moulisea <c84.srinivasan@gmail.com>
2016-03-16 11:47:41 -0400

    Added files via upload

---
+++ LoggingTest.java
@@ -0,0 +1,19 @@
+import org.apache.log4j.Logger;
+
+
+public class LoggingTest {
+	static Logger l = Logger.getLogger(LoggingTest.class.getName());
+
+	public static void main(String[] args) {          
+
+
+		l.debug("This is debug message");
+		l.info("This is info message");
+		l.warn("This is warn message");
+		l.fatal("This is fatal message");
+		l.error("This is error message");
+
+		System.out.println("Your logic executed successfully....");
+
+	}
+}
